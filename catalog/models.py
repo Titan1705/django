1fc83814e7966b5fbaf518b075949af34226fcf6
Felix <amon_ra17@mail.ru>
2024-06-10 09:06:23 +0300

    start

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование",
+        help_text="Введите наименование продукта",
+    )
+    description = models.CharField(
+        max_length=100, verbose_name="Описание", help_text="Введите описаание продукта"
+    )
+    photo = models.ImageField(
+        upload_to="products/photo",
+        blank=True,
+        null=True,
+        verbose_name="Изображение",
+        help_text="Вставте изображение продукта",
+    )
+    category = models.ForeignKey(
+        "Category",
+        on_delete=models.CASCADE,
+        verbose_name="Категория",
+        related_name="products",
+    )
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="Цена за покупку"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Дата последнего изменения"
+    )
+
+    # manufactured_at = models.DateTimeField(auto_now=True, verbose_name="Дата производства продукта")
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name", "price"]
+
+    def __str__(self):
+        return f"{self.name}, {self.description}, {self.price}"
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100, verbose_name="Категория", help_text="Введите категорию"
+    )
+    description = models.TextField(
+        verbose_name="Описание категории",
+        help_text="Введите описаание категории",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return f"{self.name}, {self.description}"
+
+
